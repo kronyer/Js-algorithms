544de5b760c5a9687455ccd9b34b3a5ab96d0245
Pedro Antonio <pedromila2000@gmail.com>
2024-09-18 11:23:42 -0300

    linked list push, removeat e getat

---
+++ LinkedList_ListaLigada/linkedList.js
@@ -0,0 +1,58 @@
+import { Node } from "./Models/index.js";
+import { defaultEquals } from "../utils/index.js";
+export default class LinkedList {
+  constructor(equalsFn = defaultEquals) {
+    this.count = 0;
+    this.head = undefined;
+    this.equalsFn;
+  }
+  push(element) {
+    const node = new Node(element);
+    let current;
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next != null) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.count++;
+  }
+
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+
+      if (index === 0) {
+        this.head = current.next;
+      } else {
+        let previous;
+        for (let i = 0; i < index; i++) {
+          previous = current;
+          current = current.next;
+        }
+        previous.next = current.next;
+      }
+      this.count--;
+      return current.element;
+    }
+    return undefined;
+  }
+  getElementAt(index) {
+    if (index >= 0 <= this.count) {
+      let node = this.head;
+
+      for (let i = 0; i < index && node != null; i++) {
+        node = node.next;
+      }
+      return node;
+    }
+    return undefined;
+  }
+}
+
+const list = new LinkedList();
+list.push(15);
+list.push(9);
